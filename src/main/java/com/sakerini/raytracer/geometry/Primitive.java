1bbe26689afe52d1a78e8a1f9b3fc7e40c46ff96
Sakerini <sakerini1999@gmail.com>
2020-11-19 13:53:17 +0300

    [Feature] Primitive abstract class added

---
+++ src/main/java/com/sakerini/raytracer/geometry/Primitive.java
@@ -0,0 +1,21 @@
+package com.sakerini.raytracer.geometry;
+
+import lombok.Getter;
+
+public abstract class Primitive {
+
+    @Getter
+    protected Vector3D[] vertices;
+
+    protected Primitive(Vector3D[] vertices) {
+        this.vertices = vertices;
+    }
+    protected Primitive(Vector3D position) {
+        vertices = new Vector3D[1];
+        vertices[0] = position;
+    }
+
+    //TODO Intersection with RAY
+    public void intersect() {}
+
+}
