9fec9ad2710a85f0b0f30dad766f3a6c2bfcf5cb
Sakerini <sakerini1999@gmail.com>
2020-11-20 17:21:18 +0300

    [Feature] Scene class added

---
+++ src/main/java/com/sakerini/raytracer/entity/Scene.java
@@ -0,0 +1,15 @@
+package com.sakerini.raytracer.entity;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class Scene {
+
+    private List<SceneObject> sceneObjects;
+
+    public Scene() {
+        //TODO IN CONSTRUCTOR SHOULD BE CONSTRUCTED SCENE INSTANCED OBJECTS
+    }
+}
