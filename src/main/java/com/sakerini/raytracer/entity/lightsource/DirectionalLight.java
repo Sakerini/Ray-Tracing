ef1e1dbfdb466f5a2efa6ad921b902d62548b38e
Sakerini <sakerini1999@gmail.com>
2020-11-20 19:44:23 +0300

    [Update] Point and Directional light sources implemented

--- src/main/java/com/sakerini/raytracer/entity/lightsource/DirectionalLight.java
+++ src/main/java/com/sakerini/raytracer/entity/lightsource/DirectionalLight.java
@@ -1,10 +1,19 @@
 package com.sakerini.raytracer.entity.lightsource;
 
 import com.sakerini.raytracer.entity.geometry.Vector3D;
+import com.sakerini.raytracer.utils.Configuration;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class DirectionalLight extends BaseLight {
 
-    public DirectionalLight(Vector3D color, float intensity) {
+    private Vector3D direction;
+
+    public DirectionalLight(Vector3D direction, Vector3D color, float intensity) {
         super(color, intensity);
+        this.setLightType(Configuration.lightType.DIRECTIONAL);
+        this.direction = direction;
     }
 }
--- src/main/java/com/sakerini/raytracer/entity/lightsource/PointLight.java
+++ src/main/java/com/sakerini/raytracer/entity/lightsource/PointLight.java
@@ -1,10 +1,26 @@
 package com.sakerini.raytracer.entity.lightsource;
 
 import com.sakerini.raytracer.entity.geometry.Vector3D;
+import com.sakerini.raytracer.utils.Configuration;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class PointLight extends BaseLight {
 
-    public PointLight(Vector3D color, float intensity) {
+    private Vector3D position;
+    private float constant;
+    private float linear;
+    private float exponent;
+
+
+    public PointLight(Vector3D position, Vector3D color, float intensity, float constant, float linear, float exponent) {
         super(color, intensity);
+        this.setLightType(Configuration.lightType.POINT);
+        this.position = position;
+        this.constant = constant;
+        this.linear = linear;
+        this.exponent = exponent;
     }
 }
